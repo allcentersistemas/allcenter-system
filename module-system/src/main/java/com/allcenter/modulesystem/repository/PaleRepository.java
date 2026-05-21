d7e763eec99664187074c52c3f7ee0860ccd5aab
arturoruizromero <sistemas@allcenter.pe>
2026-05-20 19:26:18 -0500

    mono

--- module-system/src/main/java/com/allcenter/modulesystem/repository/PaleRepository.java
+++ module-system/src/main/java/com/allcenter/modulesystem/repository/PaleRepository.java
@@ -10,40 +10,6 @@ import org.springframework.data.repository.query.Param;
 public interface PaleRepository extends JpaRepository<Pale, Long> {
     Optional<Pale> findByCodigoIgnoreCase(String codigo);
 
-    @Query(
-            """
-            SELECT p
-            FROM Pale p
-            LEFT JOIN FETCH p.sucursalOrigen
-            LEFT JOIN FETCH p.sucursalDestino
-            LEFT JOIN FETCH p.ubicacionOrigen
-            LEFT JOIN FETCH p.ubicacionDestino
-            """)
-    List<Pale> findAllWithRelations();
-
-    @Query(
-            """
-            SELECT p
-            FROM Pale p
-            LEFT JOIN FETCH p.sucursalOrigen
-            LEFT JOIN FETCH p.sucursalDestino
-            LEFT JOIN FETCH p.ubicacionOrigen
-            LEFT JOIN FETCH p.ubicacionDestino
-            WHERE p.id = :id
-            """)
-    Optional<Pale> findByIdWithRelations(@Param("id") Long id);
-
-    @Query(
-            """
-            SELECT p
-            FROM Pale p
-            LEFT JOIN FETCH p.sucursalOrigen
-            LEFT JOIN FETCH p.sucursalDestino
-            LEFT JOIN FETCH p.ubicacionOrigen
-            LEFT JOIN FETCH p.ubicacionDestino
-            WHERE UPPER(p.codigo) = UPPER(:codigo)
-            """)
-    Optional<Pale> findByCodigoIgnoreCaseWithRelations(@Param("codigo") String codigo);
 
     @Query(
             value =
