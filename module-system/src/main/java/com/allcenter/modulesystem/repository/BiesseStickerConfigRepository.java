564a251ad269d49c81126d87ade030699f1bfb98
arturoruizromero <sistemas@allcenter.pe>
2026-07-15 19:57:03 -0500

    mono

---
+++ module-system/src/main/java/com/allcenter/modulesystem/repository/BiesseStickerConfigRepository.java
@@ -0,0 +1,6 @@
+package com.allcenter.modulesystem.repository;
+
+import com.allcenter.modulesystem.model.BiesseStickerConfig;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BiesseStickerConfigRepository extends JpaRepository<BiesseStickerConfig, Long> {}
